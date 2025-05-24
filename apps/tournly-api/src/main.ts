01f8fee4c4f936a67a88faac5c4d933968daec07
Peter Lönnqvist <peter.lonnqvist@inera.se>
2025-05-24 16:15:32 +0200

    wip: prisma schema

--- apps/tournly-api/src/main.ts
+++ apps/tournly-api/src/main.ts
@@ -3,8 +3,11 @@
  * This is only a minimal backend to get started.
  */
 
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import * as path from 'path';
+
+const prisma = new PrismaClient();
 
 const app = express();
 
@@ -12,6 +15,10 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to tournly-api!' });
+});
+
+app.get('/api/users', async (req, res) => {
+  res.send(await prisma.user.findMany());
 });
 
 const port = process.env.PORT || 3333;
